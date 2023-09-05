0477aeb62e65a580a7215ffb6628857a36790d38
K01M <michail.kasyanov@yandex.ru>
2023-09-05 23:39:21 +0500

    added section advantages slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('12345');
+const swiper = new Swiper('.advantages__slider', {
+    slidesPerView: 4,
+    initialSlide: 2,
+    spaceBetween: 16,
+    centeredSlides: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
