9667ac43e35bd2534318480d140e15942de720a3
K01M <michail.kasyanov@yandex.ru>
2023-09-07 22:01:33 +0500

    added section reports

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ const swiper = new Swiper('.advantages__slider', {
     initialSlide: 2,
     spaceBetween: 16,
     centeredSlides: true,
-
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+    },
+});
+const reportsSlider = new Swiper('.reports__slider', {
+    navigation: {
+        nextEl: ".reports__slider-next",
+        prevEl: ".reports__slider-prev",
     },
 });
